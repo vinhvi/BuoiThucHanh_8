2e9e0e43a257dfdc82d5d1768c43a99ad6312de4
Vinh <vithevinh123@gmail.com>
2022-03-28 14:31:29 +0700

    finish

---
+++ app/src/main/java/com/example/myapplication/Fragment_2.java
@@ -0,0 +1,19 @@
+package com.example.myapplication;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class Fragment_2 extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View secondFragment = inflater.inflate(R.layout.fragment_2, container, false);
+        return secondFragment;
+    }
+}
